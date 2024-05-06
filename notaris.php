8d961b566375934711815e7594282c19ca1361e4
ArifSobriyanto <160447436+ArifSobriyanto@users.noreply.github.com>
2024-05-06 10:32:34 +0700

    1.1

--- notaris.php
+++ notaris.php
@@ -14,20 +14,4 @@ if ($conn->connect_error) {
     die("Koneksi gagal: " . $conn->connect_error);
 }
 
-// Ambil data dari tabel
-$sql = "SELECT * FROM modul_pembelajaran";
-$result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    // Ubah hasil query ke dalam bentuk array
-    $rows = array();
-    while($row = $result->fetch_assoc()) {
-        $rows[] = $row;
-    }
-    // Tampilkan data dalam format JSON
-    echo json_encode($rows);
-} else {
-    echo "0 results";
-}
-$conn->close();
-?>
